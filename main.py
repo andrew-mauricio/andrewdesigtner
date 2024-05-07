9db5dc6fb51cfeb4808b96f9ae0e7622b69fcc3b
andrew-mauricio <169017090+andrew-mauricio@users.noreply.github.com>
2024-05-06 22:29:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
-
 app = Flask(__name__, static_url_path='/static')
 
 
@@ -14,4 +12,4 @@ def usuarios(nome_usuario):
     return render_template("usuarios.html",nome_usuario=nome_usuario)
 
 if __name__=="__main__":
-    app.run(port=10000, host='0.0.0.0', debug=True)
+    app.run(port=8085, host='0.0.0.0', debug=True, threaded=True)
